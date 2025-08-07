39099501f0e55f474b6e91c6e32de71722e5f30e
Abhishek Harish <itsjustabhii@gmail.com>
2025-08-07 22:25:44 +0530

    Find an element within the array and return the index

---
+++ findElement.js
@@ -0,0 +1,13 @@
+//Write a function to find an element within the array and return the index. If not found, return -1
+
+let array = [10,76,45,923,463,8,49]
+function searchElement(array, target){
+    for(let i = 0; i<array.length;i++){
+        if (array[i] == target){
+            return i
+        } 
+    }
+    return -1
+}
+
+console.log(searchElement(array, 45))
