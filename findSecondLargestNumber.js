010c93d1428031ad37d271d999901fa24160aa56
Abhishek Harish <itsjustabhii@gmail.com>
2025-08-08 10:38:46 +0530

    To find the second largest number in an array

---
+++ findSecondLargestNumber.js
@@ -0,0 +1,20 @@
+//Find the second largest number in an array
+//Need to compare the array elements with both the counters/variables
+
+let arr = [4387,7363,97,639,9353,46330,73947,48763,877,403,9,2.4,5,7,4,]
+let largest = -Infinity
+let secondLargestNumber = -Infinity
+
+function secondLargest(arr){
+    for(let i=0; i<arr.length; i++){
+        if(arr[i]>largest){
+            secondLargestNumber = largest
+            largest = arr[i]
+        } else if (arr[i] > secondLargestNumber){
+            secondLargestNumber = arr[i]
+        }
+    }
+    return secondLargestNumber
+}
+
+console.log(secondLargest(arr))
