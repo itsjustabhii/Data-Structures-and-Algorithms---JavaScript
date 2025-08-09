db948b9cf8d396b0a6da58321a2c47cf81d14d12
Abhishek Harish <itsjustabhii@gmail.com>
2025-08-09 13:18:45 +0530

    To print star in increasing order

---
+++ increasingStarPattern.js
@@ -0,0 +1,11 @@
+//Write a code to print increasing star pattern
+
+let n = 4
+
+for(let i=0; i<n; i++){
+    let row = ' '
+    for(let j=0; j<i+1; j++){ //During ever i iteration, 1 star of j loop is printed. thats why j<i+1
+        row = row + '*'
+    }
+    console.log(row)
+}
