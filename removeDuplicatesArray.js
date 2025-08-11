9b9855654ae593b3e4053930c97696c7f76256bf
Abhishek Harish <itsjustabhii@gmail.com>
2025-08-11 09:58:49 +0530

    removes duplicate numbers in an array

--- removeDuplicatesArray.js
+++ removeDuplicatesArray.js
@@ -7,7 +7,7 @@ let nums = [0,0,1,1,1,2,2,3,3,4]
 function removeDuplicates(nums){
     let x = 0
     for(let i=0; i<nums.length; i++){
-        if(nums[i] > nums[x]){ //Checks if the i pointer is greated than the x pointer
+        if(nums[i] > nums[x]){ //Checks if the value in i pointer is greated than the x pointer
             x = x + 1   //shifts the x point to the next place
             nums[x] = nums[i] //updates the value of x 
         }
