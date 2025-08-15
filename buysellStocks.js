b8cff67152397ea0fd998f83280503ca1c981f1f
Abhishek Harish <itsjustabhii@gmail.com>
2025-08-15 22:05:01 +0530

    added the question statement from Leetcode

--- buysellStocks.js
+++ buysellStocks.js
@@ -1,3 +1,8 @@
+// 121. Best time to buy and sell stocks
+//You are given an array prices where prices[i] is the price of a given stock on the ith day.
+//You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+//Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
 let prices = [1,5,6,7,3,3,6,9,67]
 
 function maxProfit(prices){
