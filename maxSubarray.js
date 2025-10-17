319a3ff933fcb3056cf2d5ff65e99ad3e4c2c161
Abhishek Harish <itsjustabhii@gmail.com>
2025-10-17 10:45:05 +0530

    to find the maximum subarray

---
+++ maxSubarray.js
@@ -0,0 +1,25 @@
+/**
+ * 53. Maximum Subarray
+  Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+Example 1:
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+ */
+
+let nums = [-2,1,-3,4,-1,2,1,-5,4]
+function maxSubArray(nums){
+    let currentSum = nums[0]
+    let maxSum = nums[0]
+
+    for(let i=1; i<nums.length; i++){
+
+        currentSum = Math.max(nums[i], currentSum+nums[i])
+
+        maxSum = Math.max(currentSum, maxSum)
+    }
+    return maxSum
+}
+
+console.log(maxSubArray(nums))
