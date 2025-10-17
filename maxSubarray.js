fb4e848fe117be8d38cbd1016a3ece1b7573678f
Abhishek Harish <itsjustabhii@gmail.com>
2025-10-17 12:45:22 +0530

    added comments

--- maxSubarray.js
+++ maxSubarray.js
@@ -10,13 +10,14 @@ Explanation: The subarray [4,-1,2,1] has the largest sum 6.
 
 let nums = [-2,1,-3,4,-1,2,1,-5,4]
 function maxSubArray(nums){
-    let currentSum = nums[0]
-    let maxSum = nums[0]
+    let currentSum = nums[0] // current running sum
+    let maxSum = nums[0]  // max sum so far
 
     for(let i=1; i<nums.length; i++){
-
+        // Either extend the current subarray or start a new one
         currentSum = Math.max(nums[i], currentSum+nums[i])
 
+        // Update global maximum if needed
         maxSum = Math.max(currentSum, maxSum)
     }
     return maxSum
