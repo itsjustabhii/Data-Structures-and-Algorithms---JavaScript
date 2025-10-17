df03d998d4e8c4ff4ad47c36e3b210188b8cc886
Abhishek Harish <itsjustabhii@gmail.com>
2025-10-17 10:17:22 +0530

    product of array except self

--- productofarrayexceptself.js
+++ productofarrayexceptself.js
@@ -6,3 +6,26 @@ The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit int
 
 You must write an algorithm that runs in O(n) time and without using the division operation.
 */
+
+var nums = [1,2,3,4]
+function productExceptSelf(nums){    
+    let n = nums.length
+    let result = new Array(n).fill(1) // initialize the new array with 1
+
+    //calculate prefix
+    let prefix = 1
+    for(let i = 0; i<n; i++){
+        result[i] = prefix
+        prefix *= nums[i]
+    }
+
+    //calculate suffix
+    let suffix = 1
+    for(let i = n-1; i >= 0; i--){
+        result[i] *= suffix
+        suffix *= nums[i]
+    }
+    return result
+}
+
+console.log(productExceptSelf(nums))
