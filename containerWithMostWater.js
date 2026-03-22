e29342c83553bf79cf5c126fbb04310b44b7629b
Abhishek Harish <itsjustabhii@gmail.com>
2026-03-22 11:48:49 +0530

    move the pointer with the smaller height to try increasing the area

--- containerWithMostWater.js
+++ containerWithMostWater.js
@@ -6,3 +6,25 @@ You are given an integer array heights where heights[i] represents the height of
 You may choose any two bars to form a container. Return the maximum amount of water a container can store.
  */
 
+function mostWater(height){
+    let left = 0
+    let right = height.length-1
+    let maxArea = 0
+
+    while(left < right){
+        let h = Math.min(height[left], height[right])
+        let w = right - left
+        area = h * w
+
+        maxArea = Math.max(maxArea, area)
+
+        if(height[left] < height[right]){
+            left++
+        } else{
+            right--
+        }
+    }
+    return maxArea
+}
+
+console.log(mostWater([1,8,6,2,5,4,8,3,7]))
