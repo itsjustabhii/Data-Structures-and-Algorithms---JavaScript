acd330f6facab4228b4fc7a1404a2a2a3cb608f2
Abhishek Harish <itsjustabhii@gmail.com>
2026-03-25 09:48:10 +0530

    longest substring without repeating characters

---
+++ longestSubstring.js
@@ -0,0 +1,10 @@
+/**
+ * Longest Substring Without Repeating Characters
+Medium
+
+Given a string s, find the length of the longest substring without duplicate characters.
+
+A substring is a contiguous sequence of characters within a string.
+
+Sliding window pattern
+ */
