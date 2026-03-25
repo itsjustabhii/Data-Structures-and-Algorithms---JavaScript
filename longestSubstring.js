28cd6387782e70e98efa05be133681836c6bd353
Abhishek Harish <itsjustabhii@gmail.com>
2026-03-25 10:01:09 +0530

    added comments - uses sliding window pattern

--- longestSubstring.js
+++ longestSubstring.js
@@ -8,3 +8,22 @@ A substring is a contiguous sequence of characters within a string.
 
 Sliding window pattern
  */
+
+function longestSubstring(s){
+    let set = new Set() //Set to store unique characters in the current window
+    let left = 0
+    let maxLength = 0
+
+    for(let right = 0; right<s.length; right++){ //Right pointer to expand the window
+        //Remove duplicates from the set until we can add the current character
+        while(set.has(s[right])){
+            set.delete(s[left])
+            left++
+        }
+        set.add(s[right]) //Add the current character to the set
+        maxLength = Math.max(maxLength, right-left +1) //Update the maximum length of the substring
+    }
+    return maxLength
+}
+
+console.log(longestSubstring("abcabcbb"))
