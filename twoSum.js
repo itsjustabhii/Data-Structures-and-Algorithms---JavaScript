db9605a1a8ca32bd70937222d41c42a571673133
Abhishek H <itsjustabhii@gmail.com>
2026-05-23 12:41:32 +0530

    To check for time complexity

--- twoSum.js
+++ twoSum.js
@@ -5,6 +5,8 @@ Given an array of integers nums and an integer target, return indices of the two
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 You can return the answer in any order.
+
+Check for time complexity O(n^2) and space complexity O(1)
 */
 
 function twoSum(arr,target){
